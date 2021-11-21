b1b19be37f182b645253c50d8cd49dad3aafa57f
sksurendrakmr <sksurendrakmr@gmail.com>
2021-11-21 20:22:08 +0530

    useQuery on click

--- src/component/ReactQuerySuperheros.js
+++ src/component/ReactQuerySuperheros.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { useQuery } from "react-query";
 import axios from "axios";
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 /**
  *  we use useQuery hook for all our data fetching needs.
  * useQuery hook requires atleast two arguments.
- * 1) unique key to identify this query
+ * 1) unique key to identify this query (similar to how we would have unique id for every row in a database)
  * 2)accepts a function that returns a promise.
  *
  * With react query, we don't have to manage react state based on the effect.
@@ -22,15 +22,13 @@ const fetchSuperheros = () => {
   return axios.get("http://localhost:4000/superheros");
 };
 const ReactQuerySuperheros = () => {
-  const { isLoading, data, isError, error, isFetching } = useQuery(
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     "super-heros",
     fetchSuperheros,
-    { refetchInterval: 2000, refetchIntervalInBackground: 1000 }
+    { enabled: false }
   );
 
-  console.log({ isLoading, isFetching });
-
-  if (isLoading) {
+  if (isLoading || isFetching) {
     return (
       <Typography variant='h2' align='center'>
         Loading...
@@ -50,6 +48,7 @@ const ReactQuerySuperheros = () => {
       <Typography variant='h2' align='center' gutterBottom={true}>
         React Query Superheros
       </Typography>
+      <Button onClick={refetch}>Fetch Superheros</Button>
       {data?.data.map((hero) => {
         return (
           <Typography key={hero.id} variant='body1'>
